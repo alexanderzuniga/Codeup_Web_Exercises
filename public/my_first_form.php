709ad761542cb0519e4f1d6d4b7f6d10e91c0eb7
Alex Zuniga <guest@Guests-MacBook-Air.local>
2014-05-22 15:24:13 -0500

    View var values in forms, applying GET or POST

---
+++ public/my_first_form.php
@@ -0,0 +1,17 @@
+<?php 
+	var_dump($_GET);
+	var_dump($_POST);
+?>
+<form method="POST" >
+    <p>
+        <label for="username">Username</label>
+        <input id="username"name="crazyman90" type="text">
+    </p>
+    <p>
+        <label for="password"> Password</label>
+        <input id="password" name="codeup" type="password">
+    </p>
+    <p>
+        <input type="Submit">
+    </p>
+</form>
