74faaeadec4d067b399161ef931dafa6200f9c17
Alex Zuniga <guest@Guests-MacBook-Air.local>
2014-05-23 09:32:52 -0500

    adjusted submit to Button. Added "placeholder" attribute to name and password

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -2,14 +2,15 @@
 	var_dump($_GET);
 	var_dump($_POST);
 ?>
-<form method="POST" >
+<form method="POST">
     <p>
-    	<label>Username: <input id="username"name="crazyman90" type="text" placeholder="Username Here" autofocus></label>
+    	<label>Username:  <input id="username" name="crazyman90" type="text" placeholder="Username Here" autofocus></label>
     </p>
     <p>
-        <input id="password" name="codeup" type="password" placeholder="Password Here">
+        <label for = "Password">Password: </label>
+        	<input id="password" name="codeup" type="password" placeholder="Password Here"></label>
     </p>
     <p>
-        <input type="Submit" value="Log In">
+        <button type="submit"> Login </button>
     </p>
 </form>
