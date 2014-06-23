dc0a218def3492d17dd3d0fbdbd71964faf4e14f
Alex Zuniga <guest@Alexs-MBA.local>
2014-06-23 14:33:53 -0500

    Added buttons with if statments to utilize pagination between parks for 4 at a time.

--- public/national_parks.php
+++ public/national_parks.php
@@ -2,31 +2,55 @@
 <html>
 <head>
 	<title>National Parks</title>
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
 </head>
 <body>
 	<? 
+	function getOffset() {
+		$page = isset($_GET['page']) ? $_GET['page'] : 1;
+		return ($page - 1) * 4;
+	}
 	$dbc = new PDO('mysql:host=127.0.0.1;dbname=national_park_db', 'root', '');
-	$stmt = $dbc->query('SELECT * FROM national_park');	
-	$array = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	$dbc-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	$query = 'SELECT * FROM national_park LIMIT 4 OFFSET ' .  getOffset();
+
+	$parks = $dbc->query($query)->fetchAll(PDO::FETCH_ASSOC);
+
+	$count = $dbc-> query('SELECT count(*) FROM national_park')->fetchColumn();
+
+	$numPages = ceil($count / 4);
+
+	$page = isset($_GET['page']) ? $_GET['page'] : 1;
+
+	$nextPage = $page + 1;
+
+	$prevPage = $page - 1;
 ?>
-	<table>
-		<tr>
-			<th>Name</th>
-			<th>Location</th>
-			<th>Date Established</th>
-			<th>Area in Acres</th>
-		</tr>	
-		<tr>	
-<? foreach ($array as $key => $value) : ?> 
-		<td> <?= $value['name'] ?></td> 
-		<td> <?= $value['location'] ?></td> 
-		<td> <?= $value['date_established'] ?></td> 
-		<td> <?= $value['area_in_acres'] ?></td> 
-
-
-		</tr>
-<? endforeach; ?>
-		
+<div #table>
+<table>
+	<tr>
+		<th>Name</th>
+		<th>Location</th>
+		<th>Date Established</th>
+		<th>Area in Acres</th>
+	</tr>	
+	<tr>	
+	<? foreach ($parks as $park) : ?> 
+		<td> <?= $park['name'] ?> 			  </td> 
+		<td> <?= $park['location'] ?> 		  </td> 
+		<td> <?= $park['date_established'] ?> </td> 
+		<td> <?= $park['area_in_acres'] ?>    </td> 
+	</tr>
+<? endforeach; ?>	
 </table>
+<div>
+<? if ($prevPage > 0) : ?>
+<button><a href= "?page=<?=$prevPage; ?>"> Previous </button>
+<? endif ?>
+<? if ($nextPage < 4) : ?>
+<button><a href= "?page=<?=$nextPage; ?>"> Next </button>
+<?endif?>
 </body>
 </html>
