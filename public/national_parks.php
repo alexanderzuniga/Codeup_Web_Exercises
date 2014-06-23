24de33cf07356357045d47de7fcd6d79dadc6779
Alex Zuniga <guest@Alexs-MBA.local>
2014-06-23 11:43:37 -0500

    Added other cols. for other items from database.

--- public/national_parks.php
+++ public/national_parks.php
@@ -1,11 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<? 
-
-
-
-	?>
 	<title>National Parks</title>
 </head>
 <body>
@@ -13,19 +8,22 @@
 	$dbc = new PDO('mysql:host=127.0.0.1;dbname=national_park_db', 'root', '');
 	$stmt = $dbc->query('SELECT * FROM national_park');	
 	$array = $stmt->fetchAll(PDO::FETCH_ASSOC);
-	
-
-	?>
+?>
 	<table>
 		<tr>
 			<th>Name</th>
 			<th>Location</th>
-			<th>Date Est.</th>
+			<th>Date Established</th>
 			<th>Area in Acres</th>
 		</tr>	
 		<tr>	
 <? foreach ($array as $key => $value) : ?> 
 		<td> <?= $value['name'] ?></td> 
+		<td> <?= $value['location'] ?></td> 
+		<td> <?= $value['date_established'] ?></td> 
+		<td> <?= $value['area_in_acres'] ?></td> 
+
+
 		</tr>
 <? endforeach; ?>
 		
